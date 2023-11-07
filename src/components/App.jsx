86effb439a8feb83d0ef7d461faca83f8ff76b31
GusarovaYuliya <gusarovayuliya25@gmail.com>
2023-11-08 02:09:10 +0300

    add-pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
+import Cast from 'pages/Cast';
+import Home from 'pages/Home';
+import MovieDetails from 'pages/MovieDetails';
+import Movies from 'pages/Movies';
+import Reviews from 'pages/Reviews';
+import { NavLink, Routes } from 'react-router-dom';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
+    <div>
+      <header>
+        <NavLink to="/trending/get-trending">Home</NavLink>
+        <NavLink to="/search/search-movies">Movies</NavLink>
+        <NavLink to="/movies/get-movie-details">MovieDetails</NavLink>
+        <NavLink to="/movies/get-movie-credits">Cast</NavLink>
+        <NavLink to="/movies/get-movie-reviews">Reviews</NavLink>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/movies/get-movie-credits" element={<Cast />} />
+          <Route path="/trending/get-trending" element={<Home />} />
+          <Route path="/search/search-movies" element={<Movies />} />
+          <Route path="/movies/get-movie-details" element={<MovieDetails />} />
+          <Route path="/movies/get-movie-reviews" element={<Reviews />} />
+        </Routes>
+      </main>
       React homework template
     </div>
   );
 };
+
+// /trending/get-trending список найпопулярніших фільмів на сьогодні для створення колекції на головній сторінці.
+// /search/search-movies пошук фільму за ключовим словом на сторінці фільмів.
+// /movies/get-movie-details запит повної інформації про фільм для сторінки кінофільму.
+// /movies/get-movie-credits запит інформації про акторський склад для сторінки кінофільму.
+// /movies/get-movie-reviews запит оглядів для сторінки кінофільму.
+
+//  style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101',
+//       }}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default Home;
+
+// Ключ
+// eyJhbGciOiJIUzI1NiJ9
+//   .eyJhdWQiOiI1ODFlNGUyYmNlMWVkM2Y3ZDBhMDRiYjM3YzlmZmRjZSIsInN1YiI6IjY1NGFhNjU3NjdiNjEzMDBjODQ5NDNjNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ
+//   .xG73aL7rmWglZYIgU8NHDXAbr7zwCum9J78CBh8oQYA;
+
+// API Key: 581e4e2bce1ed3f7d0a04bb37c9ffdce
+// https://api.themoviedb.org/3/movie/550?api_key=581e4e2bce1ed3f7d0a04bb37c9ffdce
